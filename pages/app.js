b59d88e9e3bcbcaeea273707b38a8017685cf640
abanouba04 <binodx10@gmail.com>
2025-03-04 19:10:11 +0200

    login

--- pages/app.js
+++ pages/app.js
@@ -23,23 +23,78 @@ const items = document.querySelectorAll(
   ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
 );
 
-ball.addEventListener("click", () => {
-  items.forEach((item) => {
-    item.classList.toggle("active");
-  });
-  ball.classList.toggle("active");
-});
+// ball.addEventListener("click", () => {
+//   items.forEach((item) => {
+//     item.classList.toggle("active");
+//   });
+//   ball.classList.toggle("active");
+// });
 
-  gsap.fromTo(
-    ".logo-text",
-    { y: 0 }, 
-    {
-      y: -10,   
-      repeat: -1,
-      yoyo: true,
-      stagger: 0.1, 
-      ease: "power1.inOut",
-      duration: 0.5
+  // gsap.fromTo(
+  //   ".logo-text",
+  //   { y: 0 }, 
+  //   {
+  //     y: -10,   
+  //     repeat: -1,
+  //     yoyo: true,
+  //     stagger: 0.1, 
+  //     ease: "power1.inOut",
+  //     duration: 0.5
+  //   }
+  // );
+
+  const form = document.getElementById('form');
+  const username = document.getElementById('username');
+  const password = document.getElementById('password');
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+  });
+
+  const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+  }
+
+  const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+  };
+
+  const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+    let isValid = true;
+
+    if(usernameValue === ''){
+      setError(username, 'Username is required!');
+      isValid = false;
+    } else {
+      setSuccess(username);
     }
-  );
+
+    if(passwordValue === '') {
+      setError(password, 'Password is required!');
+      isValid = false; 
+    } else if (passwordValue.length < 8) {
+      setError(password, 'Password must be at least 8 characters!');
+      isValid = false;
+    } else {
+      setSuccess(password);
+    }
+
+    if (isValid) {
+      window.location.href = "index.html";
+    }
+  };
 
