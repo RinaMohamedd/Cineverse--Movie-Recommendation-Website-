41c187df12c4b6f2039b24e72d6a9160dc97c564
juddyyy334 <judyahmedmohammed@gmail.com>
2025-03-04 23:08:52 +0200

    toggle functionality

--- pages/app.js
+++ pages/app.js
@@ -29,4 +29,10 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
-
+const ball =document.querySelector(".toggle-ball");
+const items=document.querySelectorAll(".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle");
+ball.addEventListener("click",()=>{
+    items.forEach(item=>{
+        item.classList.toggle("active")
+    })
+})
