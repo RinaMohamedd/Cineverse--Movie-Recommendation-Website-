2a8f9c3594245e6bb9e9ef81af2dce1c94d27e15
omarinho348 <omar2301277@miuegypt.edu.eg>
2025-03-07 20:13:07 +0200

    fixed dark mode,slider and setup other categories in navbar

--- pages/app.js
+++ pages/app.js
@@ -1,3 +1,6 @@
+
+
+
 const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list");
 
@@ -26,51 +29,6 @@ const items = document.querySelectorAll(
 ball.addEventListener("click", () => {
   items.forEach((item) => {
     item.classList.toggle("active");
-  });
+  });	
   ball.classList.toggle("active");
 });
-
-  gsap.fromTo(
-    ".logo-text",
-    { y: 0 }, 
-    {
-      y: -10,   
-      repeat: -1,
-      yoyo: true,
-      stagger: 0.1, 
-      ease: "power1.inOut",
-      duration: 0.5
-    }
-  );
-
-
-// const arrows = document.querySelectorAll(".arrow");
-// const movieLists = document.querySelectorAll(".movie-list");
-// const scrollStep = 270; // Adjust based on movie item width
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const arrows = document.querySelectorAll(".arrow");
-//     const movieLists = document.querySelectorAll(".movie-list");
-//     const scrollStep = 270; // Adjust based on movie item width
-
-//     arrows.forEach((arrow) => {
-//         arrow.addEventListener("click", function () {
-//             const movieList = this.classList.contains("right-arrow")
-//                 ? this.previousElementSibling
-//                 : this.nextElementSibling;
-            
-//             if (this.classList.contains("right-arrow")) {
-//                 movieList.style.transform = `translateX(${movieList.computedStyleMap().get("transform")[0].x.value - scrollStep}px)`;
-//             } else {
-//                 movieList.style.transform = `translateX(${movieList.computedStyleMap().get("transform")[0].x.value + scrollStep}px)`;
-//             }
-//         });
-//     });
-// });
-// const ball =document.querySelector(".toggle-ball");
-// const items=document.querySelectorAll(".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle");
-// ball.addEventListener("click",()=>{
-//     items.forEach(item=>{
-//         item.classList.toggle("active")
-//     })
-// })
