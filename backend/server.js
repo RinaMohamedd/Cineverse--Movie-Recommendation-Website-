6deb4a9c733b7362deb61eb4b526e1798d545dd6
RinaMohamedd <rina2301123@miuegypt.edu.eg>
2025-06-03 00:03:19 +0300

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+const uri = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 3000;
+
+//middleware to parse JSON files and it's important for APIs
+app.use(express.json());
+
+//to test that the server is alive
+app.get('/', (req, res) => {
+    res.send('Server Is Alive!');
+});
+
+mongoose.connect(uri)
+.then(() => console.log('MongoDB Connected'))
+.catch((err) => console.error('MongoDB Connection Error: ', err));
+
+app.listen(PORT, () => {
+    console.log('Server is on http://localhost:${PORT}');
+});
