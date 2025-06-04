e19bc56b708ae223103793883c7ea1673b3e3c0b
abanouba04 <binodx10@gmail.com>
2025-06-04 15:08:16 +0300

    Changes to ejs files and main server

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,21 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const uri = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs'); // Set template engine
 
+app.set('views', path.join(__dirname, '../frontend/pages'));
+app.use(express.static(path.join(__dirname, '../frontend/public')));
 //middleware to parse JSON files and it's important for APIs
 app.use(express.json());
 
 //to test that the server is alive
 app.get('/', (req, res) => {
-    res.send('Server Is Alive!');
+  res.render('index'); // This looks for 'frontend/pages/index.ejs'
 });
 
 mongoose.connect(uri)
