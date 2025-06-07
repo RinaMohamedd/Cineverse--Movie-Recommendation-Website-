441d16b4b69e277de8b8650291d9e29c0a2d5fc2
RinaMohamedd <rina2301123@miuegypt.edu.eg>
2025-06-07 18:29:43 +0300

    nodemon package installed

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,7 @@ const loginRoutes = require("./routes/login");
 const signupRoutes = require("./routes/signup");
 const userRoutes = require("./routes/userRoutes");
 const movieRoutes = require('./routes/movieRoutes');
-
 const uri = process.env.MONGODB_URI;
-const PORT = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs'); // Set template engine
 
@@ -52,6 +50,6 @@ http://localhost:5000/api/users/signup
 http://localhost:5000/api/users/login
 */
 
-app.listen(PORT, () => {
-console.log(`Server is on http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+console.log(`Server is on http://localhost:${process.env.PORT}`);
 });
