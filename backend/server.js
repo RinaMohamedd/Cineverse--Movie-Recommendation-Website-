15fa50eb448cc038466ef836250cc020f21b11a7
RinaMohamedd <rina2301123@miuegypt.edu.eg>
2025-06-14 05:06:59 +0300

    server update

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-const cors = require('cors');
+const cors = require('cors'); //cross-origin resource sharing
 const path = require('path');
 const session = require('express-session');
 const homeRoutes = require("./routes/home");
@@ -25,19 +25,19 @@ app.use(express.urlencoded({ extended: true }));
 // CORS setup
 app.use(cors({
     origin: ["http://localhost:5000", "http://localhost:3000"],
-    credentials: true
+    credentials: true //allows sending cookies
 }));
 
 // Session middleware setup
 app.use(session({
-    secret: 'super-secret-code',
+    secret: 'super-secret-code', //should be changed to some actual code that we'll store in .env
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: false,
+        secure: false, //true only if using https
         httpOnly: true,
         sameSite: "lax",
-        maxAge: 1000 * 60 * 60 * 24 * 2
+        maxAge: 1000 * 60 * 60 * 24 * 2 //2 days
     }
 }));
 
@@ -46,6 +46,11 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../frontend/pages'));
 app.use(express.static(path.join(__dirname, '../frontend/public')));
 
+app.use((req, res, next) => {
+    res.locals.user = req.user;
+    next();
+});
+
 // Request logging middleware
 app.use((req, res, next) => {
     console.log(req.path, req.method);
@@ -53,11 +58,27 @@ app.use((req, res, next) => {
 });
 
 // API Routes - Put these before the page routes
-app.use('/api/users', userRoutes);
+/*app.use('/api/users', userRoutes);
 app.use('/api/movies', movieRoutes);
 app.use('/api/watchlist', watchlistRoutes);
 app.use('/api/recommendation', recommendationRoutes);
+app.use("/watchlist", watchlistRoutes);*/
+//this gets all the different routes we created so they could be used on the app
+app.use("/", homeRoutes);
+app.use("/api/watchlist", watchlistRoutes);
+app.use("/login", loginRoutes);
+app.use("/signup", signupRoutes);
+app.use('/admin', adminRoutes);
+app.use('/recommendations', recomRoutes);
+app.use('/api/movies', movieRoutes);
+app.use('/', profileRoutes);
+//routes setup
+app.use('/api/users', userRoutes);
+//app.use('/api/recommendation', recomRoutes);
+app.use('/', movieRoutes);
+app.use('/api/recommendation', recommendationRoutes);
 app.use("/watchlist", watchlistRoutes);
+
 /*
 my routes now are:
 http://localhost:5000/api/users/signup
@@ -83,20 +104,9 @@ mongoose.connect(uri)
 
 /*app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');*/
->>>>>>> Stashed changes
 
 // Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({message: 'Something broke!'});
 });
-
-// MongoDB connection and server start
-mongoose.connect(uri)
-.then(() => { 
-    console.log('MongoDB Connected');
-    app.listen(process.env.PORT, () => {
-        console.log(`Server is on http://localhost:${process.env.PORT}`);
-    });
-})
-.catch((err) => console.error('MongoDB Connection Error: ', err));
